7c24d3ca9a66af423b99bc9359e0dd5085d7095f
RasulbekQadomboyev <rasqadomboyev@gmail.com>
2024-09-03 12:11:25 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,5 +1,5 @@
-# BOT_TOKEN = "7490569949:AAGd8EWL0p5UbP8hka_xcmVgMFFtiEh4fw8"
-# ADMINS = ["7010118152"]
+# BOT_TOKEN = ""
+# ADMINS = [""]
 # IP = "localhost"
 # DB_USER = "postgres"
 # DB_PASS = "12345ras"
@@ -9,11 +9,11 @@
 
 
 
-BOT_TOKEN = "7120003578:AAGSNYznJLDWUQehsUmSRr6hMDl5nqJed9s"
-ADMINS = ["7010118152"]
-IP = "localhost"
+BOT_TOKEN = ""
+ADMINS = [""]
+IP = ""
 
-DB_USER = "botlaru1_barakaTC_user"
-DB_PASS = "barakaTC_user"
-DB_NAME = "botlaru1_barakaTC"
-DB_HOST = "localhost"
+DB_USER = ""
+DB_PASS = ""
+DB_NAME = ""
+DB_HOST = ""
